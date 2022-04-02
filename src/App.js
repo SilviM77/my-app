76d72ec39952ab0e8f145d15ca107a6729450768
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-02 12:17:11 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import "./App.css";
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
   <footer>
     This project was created by Silvia Marconi and is
   <a href="https://github.com/SilviM77/my-react-weather-app"
   target="_blank"
   > 
-  open-sourced on GitHub
+    open-sourced on GitHub
   </a>
   </footer>
+    </div>
     </div>
     );
 }
