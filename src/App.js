0792eb1a72567245f069d0dcc523ca21cd941e93
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-02 10:56:22 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+  <footer>
+    This project was created by Silvia Marconi and is
+  <a href="https://github.com/SilviM77/my-react-weather-app"
+  target="_blank"
+  > 
+  open-sourced on GitHub
+  </a>
+  </footer>
+    </div>
+    );
 }
 
