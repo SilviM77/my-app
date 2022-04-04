118211df3847c9952a56726d6ff067a726e1e857
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-03 21:37:17 -0400

    Created new component to display relevant temp in different unit/metric

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -17,20 +18,19 @@ export default function WeatherInfo(props) {
       <div className="col-6">
         <div className="clearfix">
             <div className="float-left">
-            <WeatherIcon code={props.data.icon} />
-       
+            <WeatherIcon code={props.data.icon} size={52} />
+       </div>
+
         <div className="float-left">
-        <span className="temperature">{Math.round(props.data.temperature)}</span>
-        <span className="unit">˚C</span>
+            <WeatherTemperature celsius={props.data.temperature} />
       </div>
       </div>
       </div>
       <div className="col-6">
         <ul>
           <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.datawind} km/h</li>
+          <li>Wind: {props.data.wind} km/h</li>
         </ul>
-      </div>
     </div>
     </div>
     </div>
