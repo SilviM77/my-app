c266b47cdf3f9b40ff62507d8324c7c66641bdc4
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-09 10:47:39 -0400

    Updated forecast with real data from API

--- src/Weather.js
+++ src/Weather.js
@@ -1,69 +1,71 @@
 import React, { useState } from "react";
-import WeatherInfo from "./WeatherInfo.js";
-import WeatherForecast from "./WeatherForecast.js";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
-
 export default function Weather(props) {
-const [weatherData, setWeatherData] = useState({ready: false});
-const [city, setCity] = useState(props.defaultCity);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-function handleResponse(response) {
-  setWeatherData({
-    ready: true,
-    coordinates: response.data.coord,
-    temperature: response.data.main.temp,
-    description: response.data.weather[0].description,
-    icon: response.data.weather[0].icon,
-    humidity: response.data.main.humidity,
-    wind: response.data.wind.speed,
-    city: response.data.name,
-    date: new Date(response.data.dt * 1000),
-  
-  });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  search();
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-function handleCityChange(event) {
-setCity(event.target.value);
-}
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-function search() {
-  const apiKey = "b3e1522d82584110f6073765cb9a8c79";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-}
+  function search() {
+    const apiKey = "bd8267e0e5535d9938ff03155812f193";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-if (weatherData.ready) {
-return (
-  <div className="Weather">
-    <form onSubmit={handleSubmit}>
-    <div className="row">
-      <div className="col-9">
-      <input type="search" 
-      placeholder="Enter a city..."
-      className="form-control"
-      autoFocus="on"
-      onChange={handleCityChange}
-      />
-    </div>
-    <div className="col-3">
-    <input 
-    type="submit" value="search" className="btn btn-primary w-100"/>
-</div>
-</div>
-</form>
-<WeatherInfo data={weatherData} />
-<WeatherForecast coordinates={weatherData.coord} />
-  </div>
-);
-} else {
-  search();
- return "Loading..."
-}
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { useState }from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
-export default function WeatherForecast (props) {
-  function handleResponse(response) {
-    console.log(response.data);
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "b3e1522d82584110f6073765cb9a8c79";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-    return  (
+  if (loaded) {
+    return (
       <div className="WeatherForecast">
-        <div className="row">
-            <div className="col">
-               <div className="WeatherForecast-day">Fri</div>
-                <WeatherIcon code="01d" size={36}/>
-               <div className="WeatherForecast-temperatures">
-               <span className="WeatherForecast-temperature-max">19˚</
-               span>
-               <span className="WeatherForecast-temperature-min">10˚</
-               span>
-               </div> 
-            </div>
-        </div>
-    </div>
+        <dv className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </dv>
+      </div>
     );
+  
+  
+  } else { 
+    let apiKey = "bd8267e0e5535d9938ff03155812f193";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+return null;
+  }
+  
 }
