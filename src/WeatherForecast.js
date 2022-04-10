1deeff1dd041c33f5281e459d6e7c219866f533c
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-10 19:51:44 -0400

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -32,7 +31,7 @@ export default function WeatherForecast(props) {
             ); 
           } else {
             return null;
-            
+
           }
           })}
       </div>
